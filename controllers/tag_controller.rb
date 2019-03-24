90ef55e121d2eeaad95132618a2d96740cc2147d
Rory MacGregor <you@example.com>
2019-03-24 23:47:55 +0000

    Added tags/index route.

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -3,3 +3,8 @@ require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require_relative( '../models/tag.rb' )
 also_reload( '../models/*' )
+
+get '/tags' do
+  @tags = Tag.all()
+  erb( :'tags/index' )
+end

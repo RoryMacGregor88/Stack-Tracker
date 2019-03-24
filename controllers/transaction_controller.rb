e447b61d383a8ddcd6364f90a4c1188cdb2c695b
Rory MacGregor <you@example.com>
2019-03-24 18:03:36 +0000

    Updated transactions/new form and added post /transactions in transactions_controller.

--- controllers/transaction_controller.rb
+++ controllers/transaction_controller.rb
@@ -2,6 +2,8 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require_relative( '../models/transaction' )
+require_relative( '../models/merchant' )
+require_relative( '../models/tag' )
 also_reload( '../models/*' )
 
 get '/transactions' do
@@ -13,4 +15,10 @@ get '/transactions/new' do
   @tags = Tag.all()
   @merchants = Merchant.all()
   erb( :'transactions/new' )
+end
+
+post '/transactions' do
+  transaction = Transaction.new( params )
+  transaction.save()
+  redirect to ("/transactions")
 end
